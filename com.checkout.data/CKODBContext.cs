83009bc6de7cc7f1f66fa17bd6dc6d090ef7ddf8
Hilal Yazbek <hyazbek@psa.gov.qa>
2022-06-05 11:55:05 +0300

    updated initial db migration

--- com.checkout.data/CKODBContext.cs
+++ com.checkout.data/CKODBContext.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace com.checkout.data
 {
-    public class CKODBContext: DbContext
+    public class CKODBContext : DbContext
     {
         public CKODBContext()
         {
@@ -14,7 +14,7 @@ namespace com.checkout.data
         public CKODBContext(DbContextOptions<CKODBContext> options)
             : base(options)
         {
-            
+
         }
 
         public DbSet<CardDetails> Cards { get; set; }
@@ -24,7 +24,7 @@ namespace com.checkout.data
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            
+
             string conn = "Server=localhost\\SQLEXPRESS;Database=CKODB;Trusted_Connection = True; MultipleActiveResultSets = true";
             if (!options.IsConfigured)
             {
@@ -39,6 +39,7 @@ namespace com.checkout.data
             builder.Entity<Currency>().ToTable("Currencies");
             builder.Entity<Merchant>().ToTable("Merchants");
             builder.Entity<Transaction>().ToTable("Transactions");
+            builder.Entity<CardDetails>().ToTable("Cards");
 
             builder.Entity<CardDetails>()
             .Property(cd => cd.CardDetailsID)
@@ -53,14 +54,20 @@ namespace com.checkout.data
                 );
 
             builder.Entity<Merchant>().HasData(
-                    new Merchant { Id = Guid.NewGuid(), Name = "Github", Country = "USA" },
+                    new Merchant { Id = Guid.Parse("1c4352e9-beb6-4c7f-8bfc-9263de60238b"), Name = "Github", Country = "USA" },
                     new Merchant { Id = Guid.NewGuid(), Name = "Amazon", Country = "USA" },
                     new Merchant { Id = Guid.NewGuid(), Name = "Farfetch", Country = "UK" }
+                );
+            builder.Entity<CardDetails>().HasData(
+                    new CardDetails { CardDetailsID = 1, CardNumber = "123456789", Cvv = "111", ExpiryMonth = "11", ExpiryYear = "2024", HolderName = "Valid Card" }
+                );
+            builder.Entity<Transaction>().HasData(
+                new Transaction() { TransactionID = Guid.Parse("ed9a5b76-b5cc-46f6-9372-7657a2812158"), MerchantID = Guid.Parse("1c4352e9-beb6-4c7f-8bfc-9263de60238b"), CardDetailsID = 1, Amount = new Decimal(322), CurrencyID = 1, StatusCode = "C_00001", Status = "Created" }
                 );
 
             #endregion
         }
 
-        
+
     }
 }
